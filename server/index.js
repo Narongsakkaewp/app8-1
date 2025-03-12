9f1a0fb9856d241e3cd88e0c33853043e8d27fc0
Narongsakkaewp <152575175+Narongsakkaewp@users.noreply.github.com>
2025-03-12 14:38:11 +0700

    form post

--- server/index.js
+++ server/index.js
@@ -2,20 +2,26 @@ const express = require('express')
 const app = express()
 const port = 8000
 
-app.get('/',(request, response) => {
-    response.send(`
-        <!doctype html>
-        <html>
-        <head>
-            <title>Node.JS</title>
-        </head>
-        <body>
-            <h3>Welcome to Node.js</h3>
-            <b>Express.js Fast, Unopinionated, Minimalist <br> 
-            Web Frame for Node.js</b>
-        </body>
-        </html>
-    `)
-    response.end()
+app.use(express.urlencoded({extended: true}))
+app.use(express.json())
+app.use(express.static('public'))
+
+app.post('/api/form-post', (request, response) => {
+    let name = request.body.name || ''
+    let email = request.body.email || ''
+    let msg = request.body.message || ''
+    let text = `
+    <table border="1">
+        <caption>ข้อมูลที่ส่งไป</caption>
+        <tr><td>ชื่อ:</td><td>${name}</td></tr>
+        <tr><td>อีเมล:</td><td>${email}</td></tr>
+        <tr><td>ข้อความ:</td><td>${msg}</td></tr>
+    </table>
+    `
+
+    response.send(text)
 })
-app.listen(port, () => console.log('Server Listening on Port ' + port))
+
+app.listen(port, () =>{
+    console.log('server listening on port' + port)
+})
